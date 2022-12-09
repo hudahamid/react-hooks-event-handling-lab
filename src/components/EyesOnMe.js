59ddfe2d2a4c7f40b6b02c07e9241fa615945320
hudahamid <huda.00091581@gmail.com>
2022-12-09 22:22:09 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+
+     const focusHandling = ()=>{
+        console.log('Good!');
+     }
+   
+     const blourHandling = ()=>{
+        console.log('Hey! Eyes on me!');
+     }
+       return (
+           <div>
+              <button onFocus={ focusHandling} onBlur={blourHandling }> Eyes on me</button>
+           </div>
+       )
+   }
+   
+   export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+ const Changehandling   = () => {
+     console.log("Entering password...");
+ }
+
     return (
-        <div></div>
+        <div>
+            <input  type="password" onChange={Changehandling}></input>
+        </div>
     )
 }
 
